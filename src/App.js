8c27238da219d1ce52950a00e782a80e69d82954
Ogheneochuko Pedro <pedroogheneochuko@gmail.com>
2024-01-14 20:30:50 +0100

    testing deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
+import { Sky } from "drei";
 import React from "react";
 import { Canvas } from "react-three-fiber";
-import { Sky } from "drei";
 import { Physics } from "use-cannon";
 
-import { Ground } from "./components/Ground";
 import Cubes from "./components/Cubes";
-import { Player } from "./components/Player";
+import { Ground } from "./components/Ground";
 import { Hud } from "./components/Hud";
+import { Player } from "./components/Player";
 
 function App() {
   return (
-    <Canvas shadowMap sRGB>
-      <Sky distance={[90000]} sunPosition={[300, 100, 300]} />
-      <ambientLight intensity={0.25} />
-      <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
-      <Hud position={[0, 0, -2]} />
-      <Physics gravity={[0, -30, 0]}>
-        <Ground position={[0, 0.5, 0]} />
-        <Player position={[120, 3, 70]} />
-        <Cubes />
-      </Physics>
-    </Canvas>
+    <>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+        }}
+      >
+        <p>Test</p>
+      </div>
+      <Canvas
+        shadowMap
+        sRGB
+      >
+        <Sky
+          distance={[90000]}
+          sunPosition={[300, 100, 300]}
+        />
+        <ambientLight intensity={0.25} />
+        <pointLight
+          castShadow
+          intensity={0.7}
+          position={[100, 100, 100]}
+        />
+        <Hud position={[0, 0, -2]} />
+        <Physics gravity={[0, -30, 0]}>
+          <Ground position={[0, 0.5, 0]} />
+          <Player position={[120, 3, 70]} />
+          <Cubes />
+        </Physics>
+      </Canvas>
+    </>
   );
 }
 
